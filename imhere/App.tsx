0a6e58df2a81b0b4d9bd645588e30749e6217757
tiago <tiagoamrein@gmail.com>
2023-04-21 12:54:56 -0300

    iniciando repo

---
+++ imhere/App.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native"
+export default function App(){
+
+  return(
+    <View>
+    <Text>Criando componentes</Text>
+    <Text>Criando componentes</Text>
+    </View>
+  )
+}
